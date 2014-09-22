5d3133faa99f370e523a7f6cab1bed07a3578d40
misa5555 <misa.gk@icloud.com>
2014-09-22 12:51:13 -0700

    update production.rb for assets on heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BigStart::Application.config.secret_key_base = 'fd0b72ae7bbe4e8967a0ed6e385d1c778313c20c9e9e53b6891408121d90db37185a23c7e084b4480d3ca8699f6fb26389057b9a55d6548866561b5cb8a3998f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BigStart::Application.config.session_store :cookie_store, key: '_big_start_session'
