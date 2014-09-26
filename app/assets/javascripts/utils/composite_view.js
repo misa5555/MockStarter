713b84708335c33d1c2e3bd6a1e1c49d3ccf5c31
misa5555 <misa.gk@icloud.com>
2014-09-26 11:04:00 -0700

    finished comment view

--- app/assets/javascripts/utils/composite_view.js
+++ app/assets/javascripts/utils/composite_view.js
@@ -6,7 +6,7 @@ Backbone.CompositeView = Backbone.View.extend({
 
   attachSubview: function (selector, subview) {
     this.$(selector).append(subview.$el);
-    // subview.delegateEvents();
+    subview.delegateEvents();
     
     if (subview.attachSubviews) {
       subview.attachSubviews();
