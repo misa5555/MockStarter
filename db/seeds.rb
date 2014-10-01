3c9d01a1f6f1d9c19814553d78369da0f54efe49
misa5555 <misa.gk@icloud.com>
2014-10-01 16:23:45 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,21 @@
 require 'faker'
-10.times do |i|
-  User.create!(username: Faker::Name.name, password: 'password')
-end
+# generate user data
+# 10.times do |i|
+#   User.create!(username: Faker::Name.name, password: 'password')
+# end
 
-30.times do |i|
-  user = User.find( (i % 10)  + 1)
-  user.projects.create(title: Faker::Company.name, target_amount: 10000 + rand(100)*10, end_date: Date.today + (400 + rand(400)), category_id: rand(14), description: Faker::Lorem.paragraph)
-end
+# generate project data
+# 30.times do |i|
+#   user = User.find( (i % 10)  + 1)
+#   user.projects.create(title: Faker::Company.name, target_amount: 10000 + rand(100)*10, end_date: Date.today + (400 + rand(400)), category_id: rand(14), description: Faker::Lorem.paragraph)
+# end
 
 project_count = Project.all.count
 user_count = User.all.count
-(project_count).times do |i|
-  project = Project.find(i+1)
+
+# generate back data
+for i in 31..project_count  
+  project = Project.find(i)
   user_index = Array 1..user_count
   user_index.sample(1+rand(5)).each do |j|
     if project.creator.id != j
@@ -20,10 +24,10 @@ user_count = User.all.count
   end
 end
 
-(project_count).times do |i|
-  project = Project.find(i+1)
+for i in 31..project_count  
+  project = Project.find(i)
   user_num = User.all.count
   rand(5).times do
-    project.comments.create( body: Faker::Company.catch_phrase, project_id: project.id, author_id: rand(user_num))
+    project.comments.create( body: Faker::Lorem.sentence(), project_id: project.id, author_id: rand(user_num))
   end  
 end
